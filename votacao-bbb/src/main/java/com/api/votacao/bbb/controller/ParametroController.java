653a0c549bedc045f694a67981ccc7ab67009779
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-02-16 13:18:48 -0300

    Criado os endpoints(salvar e consultar)

--- votacao-bbb/src/main/java/com/api/votacao/bbb/controller/ParametroController.java
+++ votacao-bbb/src/main/java/com/api/votacao/bbb/controller/ParametroController.java
@@ -1,18 +1,36 @@
 package com.api.votacao.bbb.controller;
 
+import com.api.votacao.bbb.model.ParametroModel;
 import com.api.votacao.bbb.repository.ParametroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/parametros")
 public class ParametroController {
 
 
-    private final ParametroRepository parametroRepository;
+    private final ParametroRepository repository;
 
-    public ParametroController(ParametroRepository parametroRepository) {
-        this.parametroRepository = parametroRepository;
+    public ParametroController(ParametroRepository parametroRepository, ParametroRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/salvar")
+    public ResponseEntity<ParametroModel> salvar(@RequestBody ParametroModel parametro){
+        ParametroModel entidade = repository.save(parametro);
+        return ResponseEntity.ok(entidade);
+    }
+
+    @GetMapping("/consultar")
+    public ResponseEntity<ParametroModel>consultar(@RequestParam String chave){
+        Optional<ParametroModel> optParametro = repository.findById(chave);
+        if(optParametro.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(optParametro.get());
     }
 }
