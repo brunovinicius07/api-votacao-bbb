dfd9153f1d83e2e429a1eca44fbf024baf2f577e
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-02-16 21:27:51 -0300

    commit

---
+++ votacao-bbb-microservico/src/main/java/com/api/votacao/bbb/ms/VotacaoBbbMicroservocoApplication.java
@@ -0,0 +1,13 @@
+package com.api.votacao.bbb.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoBbbMicroservocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoBbbMicroservocoApplication.class, args);
+	}
+
+}
